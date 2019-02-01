68b808ee9050d5f9d15b805cd5186ebe551e20b6
Unknown <darkphoenix199@gmail.com>
2019-02-01 01:01:48 +0000

    v0.4
    Implemented running animation and made the particle system functional.

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -4,28 +4,107 @@ using UnityEngine;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-    public Transform AimTransform;
-    Transform sPosAimTransform;
+    [SerializeField] GameObject thePlayer;
 
-    // Use this for initialization
-    void Start () {
-        sPosAimTransform = AimTransform;
-	}
+    [Header("Movement")]
+    [SerializeField] float moveSpeed = .1f;
+    [SerializeField] SpriteRenderer spritePlayer;
+    [Header("Aiming")]
+    [SerializeField] float aimSpeed = 1;
+    [SerializeField] Transform AimRotationalPivot;
+    [SerializeField] GameObject WeaponArm;
+    [SerializeField] GameObject aimTarget;
+    Animator anim;
+    public float horAxis;
+    public float verAxis;
+
+
+    Vector3 playerScale;
+
+    void Start () 
+    {
+        playerScale = thePlayer.transform.lossyScale;
+        anim = GetComponent<Animator>();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-        Aiming();
+	void Update () 
+	{
+        Aiming();       //  TODO : Move to FixedUpdate
 	}
+
+    void FixedUpdate()
+    {
+        PlayerMovement(true);
+    }
+
+
+
+
+    void PlayerMovement(bool isEnabled)
+    {
+
+        // spritePlayer.flipX = true;
+
+        if (isEnabled)
+        {
+            horAxis = Input.GetAxis("Horizontal");
+            verAxis = Input.GetAxis("Vertical");
+            
+            thePlayer.transform.position = new Vector2(thePlayer.transform.position.x + horAxis * moveSpeed, thePlayer.transform.position.y);
+
+            if (horAxis > 0)
+            {
+                thePlayer.transform.localScale = new Vector3(playerScale.x, playerScale.y, playerScale.z); //spritePlayer.flipX = true;
+            }
+            else if (horAxis < 0)
+            {
+                thePlayer.transform.localScale = new Vector3(-playerScale.x, playerScale.y, playerScale.z); // spritePlayer.flipX = false;
+            }
+            
+            if (horAxis < 0.05 || horAxis > -0.05)
+            {
+                anim.SetFloat("speed", 0);
+            }
+
+            if (horAxis > 0.05 || horAxis < -0.05)
+            {
+                anim.SetFloat("speed", 1);
+            }
+            
+        }
+
+        
+    }
 
     void Aiming ()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) // Bool : Has weapon?
         {
-            var pos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, sPosAimTransform.position.z));
-            Debug.Log(pos);
-            // code
-            AimTransform.position = pos;
-             }
+
+            Vector2 mousePos = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+
+            mousePos.x = mousePos.x - 0.5f;
+            mousePos.y = mousePos.y - 0.5f;
+
+            mousePos *= 2;
+
+            aimTarget.transform.localPosition = mousePos;
+
+            float angle = Vector2.Angle(Vector2.up, mousePos);
+
+            Debug.Log("Angle: " + angle);
+
+            Vector3 axis = new Vector3(0, 0, 1);
+
+            Vector3 lookAtPos = Vector3.zero;
+            lookAtPos.z = aimTarget.transform.position.y;
+
+           // WeaponArm.transform.RotateAround(AimRotationalPivot.position, axis, angle);
+           WeaponArm.transform.LookAt(lookAtPos);
+
+        }
     }
 
 }
+
+
---
+++ Assets/SpawnInk.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnInk : MonoBehaviour
+{
+	public GameObject inkPuddlePrefab;
+	public ParticleSystem PS;
+	public List<ParticleCollisionEvent> collisionEvents;
+
+	// Use this for initialization
+	void Start () {
+		
+		PS = GetComponent<ParticleSystem>();
+		collisionEvents = new List<ParticleCollisionEvent>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnParticleCollision(GameObject other)
+	{
+		int i = 0;
+		int numCollisionEvents = PS.GetCollisionEvents(other, collisionEvents);
+		while (i < numCollisionEvents)
+		{
+			if (other.CompareTag("Ground") || other.gameObject.layer.ToString() == "Ground")
+			{
+				Vector3 pos = collisionEvents[i].intersection;
+				var offsetY = new Vector3(0, -0.6f, 0);
+				GameObject newInkPuddle = Instantiate(inkPuddlePrefab, pos + offsetY, Quaternion.identity);
+			}
+			i++;
+		}
+	}
+	
+	
+}
